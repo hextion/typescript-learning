41b9db1f860517d490c8c5614aa6b09909a80d64
hextion <100ishundred@gmail.com>
2020-04-23 00:48:47 +0300

    Renamed shallow to copy

--- src/make-tree/make-tree.ts
+++ src/make-tree/make-tree.ts
@@ -9,11 +9,11 @@ interface Options<T> {
 
 export function makeTree<T>(source: Array<T>, options: Options<T>): Array<T & Parental<T>> {
   const { keySelector, parentKeySelector } = options;
-  const shallow = source.map((item) => ({ ...item }));
-  return shallow
+  const copy = source.map((item) => ({ ...item }));
+  return copy
     .map((item) => {
       const key = keySelector(item);
-      let children: Array<T> | null = shallow.filter((item) => parentKeySelector(item) === key);
+      let children: Array<T> | null = copy.filter((item) => parentKeySelector(item) === key);
       children = children.length > 0 ? children : null;
       return Object.assign(item, { children }) as T & Parental<T>;
     })
