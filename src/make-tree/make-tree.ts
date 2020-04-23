0b9a4502a85737cf053f3c6ed03a773f89789b8e
hextion <100ishundred@gmail.com>
2020-04-23 18:31:48 +0300

    Used shallow

--- src/make-tree/make-tree.ts
+++ src/make-tree/make-tree.ts
@@ -1,15 +1,17 @@
+import { shallow } from "../shallow";
+
 interface Parental<T> {
   children: Array<T & Parental<T>> | null;
 }
 
-interface Options<T> {
+export interface Options<T> {
   keySelector(item: T): string | number;
   parentKeySelector(item: T): string | number | null;
 }
 
-export function makeTree<T>(source: Array<T>, options: Options<T>): Array<T & Parental<T>> {
+export function makeTree<T extends object>(source: Array<T>, options: Options<T>): Array<T & Parental<T>> {
   const { keySelector, parentKeySelector } = options;
-  const copy = source.map((item) => ({ ...item }));
+  const copy = source.map((item) => shallow(item));
   return copy
     .map((item) => {
       const key = keySelector(item);
