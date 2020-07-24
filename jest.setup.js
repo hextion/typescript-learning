c2977d44cd4b6455a9554f4876b1ef009b635b85
hextion <100ishundred@gmail.com>
2020-07-24 21:33:36 +0500

    Simple toDeepEqual code

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,14 @@
 expect.extend({
   toDeepEqual(received, expected) {
     // see https://jestjs.io/docs/en/expect#toequalvalue
-    if (Buffer.from(received).equals(Buffer.from(expected))) {
-      return {
-        message: "expected is deep equal to received",
-        pass: true,
-      };
-    } else {
-      return {
-        message: "expected is not (deep) equal to received",
-        pass: false,
-      };
-    }
+    return Buffer.from(received).equals(Buffer.from(expected))
+      ? {
+          message: "expected is deep equal to received",
+          pass: true,
+        }
+      : {
+          message: "expected is not (deep) equal to received",
+          pass: false,
+        };
   },
 });
