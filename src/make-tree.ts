99dee06250e1d76b135b7d7d1e9dc7e702fd1c73
hextion <100ishundred@gmail.com>
2020-08-28 22:19:38 +0300

    Replaced T by V

--- src/make-tree.ts
+++ src/make-tree.ts
@@ -1,24 +1,24 @@
 import { shallow } from "./shallow";
 
-export type Parental<T> = T & {
-  children: Array<Parental<T>> | null;
+export type Parental<V> = V & {
+  children: Array<Parental<V>> | null;
 };
 
-interface Options<K, T> {
-  keySelector(item: T): K;
-  parentKeySelector(item: T): K | null;
+interface Options<K, V> {
+  keySelector(item: V): K;
+  parentKeySelector(item: V): K | null;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function makeTree<K, T extends Record<string, any>>(
-  source: Array<T>,
-  { keySelector, parentKeySelector }: Options<K, T>
-): Array<Parental<T>> {
+export function makeTree<K, V extends Record<string, any>>(
+  source: Array<V>,
+  { keySelector, parentKeySelector }: Options<K, V>
+): Array<Parental<V>> {
   return source
     .map(shallow)
     .map((item, _, arr) => {
       const key = keySelector(item);
-      const children = arr.filter((item) => Object.is(parentKeySelector(item), key)) as Array<Parental<T>>;
+      const children = arr.filter((item) => Object.is(parentKeySelector(item), key)) as Array<Parental<V>>;
       return Object.assign(item, { children: children.length > 0 ? children : null });
     })
     .filter((item) => Object.is(parentKeySelector(item), null));
