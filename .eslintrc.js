c56ad8d082c19615afb78a7628881b99d4a3e274
hextion <100ishundred@gmail.com>
2020-11-06 00:00:49 +0300

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-    "prettier",
-    "prettier/@typescript-eslint",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.eslint.json"],
-  },
-  plugins: ["@typescript-eslint", "jest"],
   root: true,
-  rules: {
-    "@typescript-eslint/array-type": [
-      "error",
-      {
-        default: "generic",
-        readonly: "generic",
+  plugins: ["jest"],
+  extends: ["eslint:recommended", "plugin:jest/recommended", "plugin:jest/style", "prettier"],
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+        "plugin:jest/style",
+        "prettier",
+        "prettier/@typescript-eslint",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.eslint.json"],
       },
-    ],
-    "@typescript-eslint/method-signature-style": ["error", "method"],
-  },
+      plugins: ["@typescript-eslint", "jest"],
+      rules: {
+        "@typescript-eslint/array-type": ["error", { default: "generic", readonly: "generic" }],
+        "@typescript-eslint/method-signature-style": ["error", "method"],
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+  ],
 };
